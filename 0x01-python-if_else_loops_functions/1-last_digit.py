39734e84c0dab6a1965f75c12fd712c0a12780b6
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-07 03:37:30 +0300

    1. The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -31,3 +31,28 @@ elif number < 0:
 
 
 print(f"Last digit of {number} is {last_digit} and is {greater_less}")
+"""
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of 4205 is 5 and is less than 6 and not 0
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of -626 is -6 and is less than 6 and not 0
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of 1144 is 4 and is less than 6 and not 0
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of -9200 is 0 and is 0
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of 5247 is 7 and is greater than 5
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of -9318 is -8 and is less than 6 and not 0
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of 3369 is 9 and is greater than 5
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of -5224 is -4 and is less than 6 and not 0
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of -4485 is -5 and is less than 6 and not 0
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of 3850 is 0 and is 0
+guillaume@ubuntu:~/0x01$ ./1-last_digit.py
+Last digit of 5169 is 9 and is greater than 5
+guillaume@ubuntu:~/0x01$
+"""
