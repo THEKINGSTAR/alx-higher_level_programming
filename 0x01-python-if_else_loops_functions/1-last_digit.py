f0d7ba8c04aa9cc6618d28d67281ab727726703f
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-07 03:24:10 +0300

    1. The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,19 +6,30 @@ The output of the program should be:
 The string Last digit of, followed by
 the number, followed by
 the string is, followed by the last digit of number, followed by
-if the last digit is greater than 5: the string and is greater than 5
+if the last digit is greater than 5:
+the string and is greater than 5
 if the last digit is 0: the string and is 0
 if the last digit is less than 6 and not 0:
 the string and is less than 6 and not 0
 followed by a new line
 """
-last_digit = number /10
-if last_digit > 5:
-    greater_less = "greater than 5"
-elif last_digit == 0:
-    greater_less = "and is 0"
-elif last_digit < 5:
-    greater_less = "grater than"
+if number > 0:
+    last_digit = (number % 10)
+    if last_digit > 5:
+        greater_less = "greater than 5"
+    elif last_digit == 0:
+        greater_less = "and is 0"
+    elif last_digit < 6 and last_digit != 0:
+        greater_less = "less than 6 and not 0"
+elif number < 0:
+    tmp = number * -1
+    last_digit = (tmp % 10) * -1
+    if last_digit > 5:
+        greater_less = "greater than 5"
+    elif last_digit == 0:
+        greater_less = "and is 0"
+    elif last_digit < 5:
+        greater_less = "less than 6 and not 0"
 
 
 print(f"Last digit of {number} is {last_digit} and is {greater_less}")
