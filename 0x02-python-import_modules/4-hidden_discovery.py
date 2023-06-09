3078c6e109bacb9b31c6ef1d29a8b78e34af41bd
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-10 02:52:20 +0300

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from hidden_4
-    names = dir(platform)
-    print("{}".format(a))
+    import hidden_4.pyc
+    names = dir(hidden_4)
+    for name in names:
+        print(name)
