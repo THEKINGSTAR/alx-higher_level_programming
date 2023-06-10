a9ab1ad001ae960499e27b558128c2fcd0e460b2
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-10 08:18:40 +0300

    7. islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+# Returns True if c is lowercase
+# Returns False otherwise
+def islower(c):
+    if (ord(c) > 64) and (ord(c) < 91):
+        return (False)
+    if (ord(c) > 96) and (ord(c) < 123):
+        return (True)
