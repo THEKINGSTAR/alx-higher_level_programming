44d0c1d1b1190fc8c8c42741d9424b17fb66dda0
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-10 22:53:03 +0300

    13. Insert in sorted linked list

---
+++ 0x01-python-if_else_loops_functions/insrt_test/13-insert_number.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_node - insert function
+ *
+ * Technical interview preparation:
+ * You are not allowed to google anything
+ * Whiteboard first
+ * Write a function in C that inserts a number
+ * into a sorted singly linked list.
+ * Prototype: listint_t *insert_node(listint_t **head, int number);
+ * @head: head of the function to insert in
+ * @number: the number to insert in the functino
+ *
+ * Return: the address of the new node, or NULL if it failed
+ * listint_t *insert_node(listint_t **head, int number);
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current, *insrt, *prev;
+
+	current = *head;
+	prev = NULL;
+	if (current == NULL)
+		return (NULL);
+	insrt = malloc(sizeof(listint_t));
+	insrt->n = number;
+	/*printf("number to insert : %d\n",number);*/
+	while (current != NULL)
+	{
+		prev = current;
+		current = current->next;
+		if (prev == NULL)
+		{
+			insrt->next = prev;
+			return (insrt);
+		}
+		if (insrt->n < prev->n)
+		{
+			/*printf("inside insert\n");*/
+			insrt->next = prev;
+			prev->next = current;
+			return (insrt);
+		}
+		if (prev->n < number && current->n > number)
+		{
+			insrt->next = current;
+			prev->next = insrt;
+			return (insrt);
+		}
+
+	}
+	return (NULL);
+}
