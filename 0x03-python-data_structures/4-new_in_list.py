cff0ba859d152bb61ecce77e291add6beae11708
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-11 07:46:20 +0300

    4. Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list
+    if idx < 0:
+        return (my_list)
+    elif idx > (len(my_list) - 1):
+        return (my_list)
+    else:
+        new_list[idx] = element
+        return (new_list)
