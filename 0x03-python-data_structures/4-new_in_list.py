2515db29c371c0f1c7203f24d2415bac2eec1e3a
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-11 17:18:01 +0300

    4. Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list
+    new_list = my_list.copy()
     if idx < 0:
         return (my_list)
     elif idx > (len(my_list) - 1):
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    for ch in my_string:
+        if ch == 'c':
+            my_string = my_string - ch 
+    return (my_string)
