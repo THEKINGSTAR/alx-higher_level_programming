34dd469fe08a03a39cf5a129208ccde990669180
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-13 02:02:12 +0300

    fix 11. Delete at

--- 0x03-python-data_structures/11-main.py
+++ 0x03-python-data_structures/11-main.py
@@ -6,3 +6,20 @@ idx = 3
 new_list = delete_at(my_list, idx)
 print(new_list)
 print(my_list)
+
+
+my_list = [1, 2, 3, 4, 5]
+idx = 11
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+
+
+
+
+my_list = []
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
