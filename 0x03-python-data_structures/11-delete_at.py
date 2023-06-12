840005b18e829ead865df5b5f9ab5766e822477d
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-12 07:48:04 +0300

    11. Delete at

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+def delete_at(my_list=[], idx=0):
+    if idx < 0:
+        return (my_list)
+    n_list = []
+    my_list = my_list[:idx] + my_list[idx + 1:]
+    n_list = my_list
+    return (n_list)
