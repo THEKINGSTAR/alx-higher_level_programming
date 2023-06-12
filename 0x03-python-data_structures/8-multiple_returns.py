ddbeb7cbe9fcbec2c15efc83fb6a7b91e56b48c5
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-12 07:17:50 +0300

    8. More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,12 +2,8 @@
 
 def multiple_returns(sentence):
     frst = sentence[0]
-    lenght = 0
-    print(sentence)
+    lenght = len(sentence)
     if len(sentence) == 0:
         frst = None
-        lenght = 0
-    else:
-        lenght = len(sentence)
     str_tup = (lenght, frst)
     return (str_tup)
