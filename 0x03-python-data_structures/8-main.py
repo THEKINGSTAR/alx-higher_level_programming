7cce8ffabadf43f5413fd29b5a1d1b0a2bfd340a
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-12 07:21:43 +0300

    8. More returns

--- 0x03-python-data_structures/8-main.py
+++ 0x03-python-data_structures/8-main.py
@@ -4,3 +4,11 @@ multiple_returns = __import__('8-multiple_returns').multiple_returns
 sentence = "At school, I learnt C!"
 length, first = multiple_returns(sentence)
 print("Length: {:d} - First character: {}".format(length, first))
+
+
+
+sentence = ""
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
+
+
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    frst = sentence[0]
-    lenght = len(sentence)
+    frst = " "
+    lenght = 0
     if len(sentence) == 0:
         frst = None
+    elif len(sentence) > 0:
+        frst = sentence[0]
+        lenght = len(sentence)
+
     str_tup = (lenght, frst)
     return (str_tup)
