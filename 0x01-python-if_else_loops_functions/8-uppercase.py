605774b6601a91083f284faa11b484a6b323624c
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-13 02:11:55 +0300

    fix 8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
-'''
-Write a function that prints a string in uppercase followed by a new line.
-
-Prototype: def uppercase(str):
-You can only use no more than 2 print functions with string format
-You can only use one loop in your code
-You are not allowed to import any module
-You are not allowed to use str.upper() and str.isupper()
-'''
-
-
-def uppercase(stri):
+def uppercase(str):
     nstr = ""
     upp = ""
-    word = list(stri)
+    word = list(str)
     for c in word:
         if (ord(c) > 96) and (ord(c) < 123):
             ch = ord(c) - 32
