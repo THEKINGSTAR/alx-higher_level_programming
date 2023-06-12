9e9cc9f15880a6a363baab827c48230a75ae3a58
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-12 07:30:26 +0300

    9. Find the max

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -5,3 +5,10 @@ my_list = [1, 90, 2, 13, 34, 5, -13, 3]
 max_value = max_integer(my_list)
 print("Max: {}".format(max_value))
 
+my_list = []
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
+
+my_list = [-12, -90, -2, -13, -34, -5, -13, -3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
-    n = 0
+    n = -1000
     for m in my_list:
         if m > n:
             n = m
