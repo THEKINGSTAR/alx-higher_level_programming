6e0ead5c8eed826b316da0d2d0d49a90feaa481b
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-14 00:31:35 +0300

    OLD WITH MEMORY EFFICINCE FAILD 13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,76 +1,6 @@
 #include "lists.h"
 
-int raverse_revers(listint_t *head);
-
-/**
- * revrs_list - reverse inkd list
- *
- * @head: the head of the list
- *
- * Return: return the value of the last node in
- * in the list
- */
-listint_t *revrs_list(listint_t *head)
-{
-	listint_t *current = head, *prev = NULL, *next = NULL;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-	return (prev);
-}
-
-/**
- * _is_palindrome - check palindrome function
- *
- * Technical interview preparation:
- * You are not allowed to google anything
- * Whiteboard first
- * Write a function in C that checks if a singly linked list is a palindrome.
- * Prototype: int is_palindrome(listint_t **head);
- *
- * @head: head of the linked_list to check;
- *
- * Return: 0 if it is not a palindrome,
- * 1 if it is a palindrome
- *
- * An empty list is considered a palindrome
- *
- */
-int _is_palindrome(listint_t **head)
-{
-	int is_pm = 0, s, r;
-	listint_t *start, *reversed;
-
-	start = *head;
-	if (start == NULL || start->next == NULL)
-	{	is_pm = 1;
-		return (is_pm);
-	}
-	reversed = revrs_list(*head);
-	while (start != NULL && start == reversed)
-	{
-		s = start->n;
-		r = reversed->n;
-		printf("COMPARE BETWEEN Start Value %d , Reverse Value %d\n", s, r);
-		if (s != r)
-		{
-			printf("DIFFRENT VALUES ARE Start Value %d , Reverse Value %d\n", s, r);
-			return (is_pm);
-		}
-		start = start->next;
-		reversed = reversed->next;
-	}
-	is_pm = 1;
-	return (is_pm);
-}
-
-
-
+int odd_is_palindrome(int *arry, int lenght);
 
 /**
  * is_palindrome - check palindrome function
@@ -91,64 +21,78 @@ int _is_palindrome(listint_t **head)
  */
 int is_palindrome(listint_t **head)
 {
-	int is_pm = 0, s, r;
+	int is_pm = 0, lenght = 0, lop_lenght = 0, *arry, idx = 0;
 	listint_t *start;
 
 	start = *head;
-	if (start == NULL || start->next == NULL)
+	if (start == NULL)
 	{	is_pm = 1;
 		return (is_pm);
 	}
 	while (start != NULL)
+	{	start = start->next;
+		lenght++;
+	}
+	if (lenght == 1)
+	{	is_pm = 1;
+		return (is_pm);
+	}
+	start = *head;
+	arry = malloc(sizeof(int) * lenght);
+	while (start != NULL)
+	{	arry[idx] = start->n;
+		start = start->next;
+		idx++;
+	}
+	lop_lenght = lenght;
+	if (lenght % 2 != 0)
 	{
-		s = start->n;
-		r = raverse_revers(start);
-		/*printf("COMPARE BETWEEN Start Value %d , Reverse Value %d\n", s, r);*/
-		if (s != r)
-		{
-			/*printf("DIFFRENT VALUES ARE Start Value %d , Reverse Value %d\n", s, r);*/
+		/* is_pm = odd_is_palindrome(arry, lenght); */
+		/* free(arry); */
+		lop_lenght = lenght / 2;
+	}
+	for (idx = 0; idx < lop_lenght; idx++)
+	{
+		if (arry[idx] != arry[lenght - idx - 1])
+		{	is_pm = 0;
+			free(arry);
 			return (is_pm);
 		}
-		start = start->next;
 	}
-	free(start);
+	free(arry);
 	is_pm = 1;
 	return (is_pm);
 }
 
 /**
- * raverse_revers - reverse traverse linkd list
+ * odd_is_palindrome - check palindrome function
  *
- * @head: the head of the list
+ * Technical interview preparation:
+ * You are not allowed to google anything
+ * Whiteboard first
+ * Write a function in C that checks if a singly linked list is a palindrome.
+ * Prototype: int is_palindrome(listint_t **head);
  *
- * Return: return the value of the last node in
- * in the list
+ * @arry: array of the elements to check;
+ * @lenght: lenght of the array
+ *
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palindrome
+ *
+ * An empty list is considered a palindrome
+ *
  */
-int raverse_revers(listint_t *head)
+int odd_is_palindrome(int *arry, int lenght)
 {
-	int val;
-	listint_t *start, *prev;
+	int is_pm = 1,	idx = 0;
 
-	if (head->next == NULL)
-		return (head->n);
-
-	prev = head;
-	start = prev->next;
-	while (prev->next != NULL)
+	for (idx = 0; idx < lenght / 2; idx++)
 	{
-		/*printf("s: %d \n", prev->n);*/
-		if (start->next == NULL)
+		if (arry[idx] != arry[lenght - idx - 1])
 		{
-			val = start->n;
-			free(start);
-			prev->next = NULL;
-			continue;
+			is_pm = 0;
+			return (is_pm);
 		}
-
-		start = start->next;
-		prev = prev->next;
 	}
-	return (val);
+	return (is_pm);
 }
-
-
