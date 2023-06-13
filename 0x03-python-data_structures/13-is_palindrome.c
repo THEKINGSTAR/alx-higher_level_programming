0bd56b8c3be17a301af4655fe2c211ddf7be931f
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-14 00:06:17 +0300

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,8 +1,28 @@
 #include "lists.h"
 
-
-
 int raverse_revers(listint_t *head);
+
+/**
+ * revrs_list - reverse inkd list
+ *
+ * @head: the head of the list
+ *
+ * Return: return the value of the last node in
+ * in the list
+ */
+listint_t *revrs_list(listint_t *head)
+{
+	listint_t *current = head, *prev = NULL, *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	return (prev);
+}
 
 /**
  * is_palindrome - check palindrome function
@@ -22,6 +42,54 @@ int raverse_revers(listint_t *head);
  *
  */
 int is_palindrome(listint_t **head)
+{
+	int is_pm = 0, s, r;
+	listint_t *start, *reversed;
+
+	start = *head;
+	if (start == NULL || start->next == NULL)
+	{	is_pm = 1;
+		return (is_pm);
+	}
+	reversed = revrs_list(*head);
+	while (start != NULL)
+	{
+		s = start->n;
+		r = reversed->n;
+		/*printf("COMPARE BETWEEN Start Value %d , Reverse Value %d\n", s, r);*/
+		if (s != r)
+		{
+			/*printf("DIFFRENT VALUES ARE Start Value %d , Reverse Value %d\n", s, r);*/
+			return (is_pm);
+		}
+		start = start->next;
+		reversed = reversed->next;
+	}
+	is_pm = 1;
+	return (is_pm);
+}
+
+
+
+
+/**
+ * _is_palindrome - check palindrome function
+ *
+ * Technical interview preparation:
+ * You are not allowed to google anything
+ * Whiteboard first
+ * Write a function in C that checks if a singly linked list is a palindrome.
+ * Prototype: int is_palindrome(listint_t **head);
+ *
+ * @head: head of the linked_list to check;
+ *
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palindrome
+ *
+ * An empty list is considered a palindrome
+ *
+ */
+int _is_palindrome(listint_t **head)
 {
 	int is_pm = 0, s, r;
 	listint_t *start;
@@ -47,6 +115,7 @@ int is_palindrome(listint_t **head)
 	is_pm = 1;
 	return (is_pm);
 }
+
 /**
  * raverse_revers - reverse traverse linkd list
  *
@@ -81,3 +150,5 @@ int raverse_revers(listint_t *head)
 	}
 	return (val);
 }
+
+
