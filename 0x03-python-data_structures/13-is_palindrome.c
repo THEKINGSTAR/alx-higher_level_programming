e49585dab1806fa74f5d40065ba3df98a1deb2d8
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-13 17:52:28 +0300

    fix efficency in 13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,8 @@
 #include "lists.h"
+
+int odd_is_palindrome(int *arry, int lenght);
+
+
 /**
  * is_palindrome - check palindrome function
  *
@@ -39,27 +43,62 @@ int is_palindrome(listint_t **head)
 		idx++;	}
 	if (lenght % 2 != 0)
 	{
-		for (idx = 0; idx < lenght / 2; idx++)
-		{
-			if (arry[idx] != arry[lenght - idx - 1])
-			{	is_pm = 0;
-				free(arry);
-				free(start);
-				return (is_pm);	}
-		}
+		is_pm = odd_is_palindrome(arry, lenght);
+		free(arry);
+		return (is_pm);
 	}
 	for (idx = 0; idx < lenght; idx++)
 	{
 		if (arry[idx] != arry[lenght - idx - 1])
 		{	is_pm = 0;
 			free(arry);
-			free(start);
-			return (is_pm);		}
+			return (is_pm);
+		}
 	}
 	free(arry);
 	is_pm = 1;
-	return (is_pm);	}
+	return (is_pm);
+}
 
+/**
+ * odd_is_palindrome - check palindrome function
+ *
+ * Technical interview preparation:
+ * You are not allowed to google anything
+ * Whiteboard first
+ * Write a function in C that checks if a singly linked list is a palindrome.
+ * Prototype: int is_palindrome(listint_t **head);
+ *
+ * @arry: array of the elements to check;
+ * @lenght: lenght of the array
+ *
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palindrome
+ *
+ * An empty list is considered a palindrome
+ *
+ */
+int odd_is_palindrome(int *arry, int lenght)
+{
+	int is_pm = 1,	idx = 0;
+
+	for (idx = 0; idx < lenght / 2; idx++)
+	{
+		if (arry[idx] != arry[lenght - idx - 1])
+		{
+			is_pm = 0;
+			free(arry);
+			return (is_pm);
+		}
+	}
+	return (is_pm);
+}
+
+
+
+
+
+/*			OLD WITH  FOR TESTING WITH PRINTF() */
 /**
  * _is_palindrome - check palindrome function
  *
@@ -122,3 +161,68 @@ int _is_palindrome(listint_t **head)
 	is_pm = 1;
 	return (is_pm);
 }
+
+/**
+ * full_is_palindrome - check palindrome function
+ *
+ * Technical interview preparation:
+ * You are not allowed to google anything
+ * Whiteboard first
+ * Write a function in C that checks if a singly linked list is a palindrome.
+ * Prototype: int is_palindrome(listint_t **head);
+ *
+ * @head: head of the linked_list to check;
+ *
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palindrome
+ *
+ * An empty list is considered a palindrome
+ *
+ */
+int full_is_palindrome(listint_t **head)
+{
+	int is_pm = 0, lenght = 0, *arry, idx = 0;
+	listint_t *start;
+
+	start = *head;
+	if (start == NULL)
+	{	is_pm = 1;
+		return (is_pm);	}
+	while (start != NULL)
+	{	start = start->next;
+		lenght++;	}
+	if (lenght == 1)
+	{	is_pm = 1;
+		return (is_pm);	}
+	start = *head;
+	arry = malloc(sizeof(int) * lenght);
+	while (start != NULL)
+	{	arry[idx] = start->n;
+		start = start->next;
+		idx++;	}
+	if (lenght % 2 != 0)
+	{
+		return (odd_is_palindrome(arry, lenght));
+		for (idx = 0; idx < lenght / 2; idx++)
+		{
+			if (arry[idx] != arry[lenght - idx - 1])
+			{	is_pm = 0;
+				free(arry);
+				return (is_pm);
+			}
+		}
+	}
+	for (idx = 0; idx < lenght; idx++)
+	{
+		if (arry[idx] != arry[lenght - idx - 1])
+		{	is_pm = 0;
+			free(arry);
+			return (is_pm);
+		}
+	}
+	free(arry);
+	is_pm = 1;
+	return (is_pm);
+}
+
+
