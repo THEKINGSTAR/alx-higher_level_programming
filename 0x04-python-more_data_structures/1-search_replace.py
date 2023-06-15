dcf0a471d860197c3992e786c710c6b41b7427f5
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-15 08:08:48 +0300

    1. Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    n_list = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            n_list.append(replace)
+        n_list.append(my_list[i])
+    return (n_list)
