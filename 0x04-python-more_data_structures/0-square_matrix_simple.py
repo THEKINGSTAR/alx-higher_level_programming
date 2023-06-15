85ac4d0acedf534c13eeea7a418bf1cbeb480662
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-15 07:26:44 +0300

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     n_matx = []
+    sub = []
 
     for i in range(len(matrix)):
+        sub = []
         for j in range(len(matrix[i])):
-            n_matx.append(matrix[i][j] ** 2)
+            sub.append(matrix[i][j] ** 2)
+        n_matx.append(sub)
 
     return (n_matx)
