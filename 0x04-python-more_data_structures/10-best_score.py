ad21efde612113ff5aca8c9f96aeebad96da2b66
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-17 01:34:56 +0300

    10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    s_d = {}
+    a_dict = a_dictionary
+    if not a_dictionary:
+        return (None)
+    ###################################
+    # sort_dict = {k: v for k,
+    # v in sorted(a_dictionary.items(),
+    # key=lambda item: item[1])}
+    ###################################
+    s_d = dict(sorted(a_dict.items(), reverse=True, key=lambda item: item[1]))
+    res = next(iter(s_d))
+    return (str(res))
