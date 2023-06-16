b16470fe01d30310250ed46f9647c7ef1f7778cb
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-17 00:56:51 +0300

    7. Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    k = key
+    if k in a_dictionary:
+        a_dictionary[k] = value
+    else:
+        a_dictionary[k] = value
+
+    return (a_dictionary)
