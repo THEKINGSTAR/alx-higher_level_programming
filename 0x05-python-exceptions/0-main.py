1ce5bb4daa7a2a75e96208b0e1afe5456bd8bf33
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-26 17:14:19 +0300

    2. Print and count integers

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -9,4 +9,5 @@ nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
-
+nb_print = safe_print_list(my_list, len(my_list) + 4444)
+print("nb_print: {:d}".format(nb_print))
