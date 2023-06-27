67664f3cbdd025e59fa15a0255cc72edcf12c3ba
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-27 13:15:53 +0300

    4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,5 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
         except IndexError:
             print("out of range")
-        new_list.append(c)
+        finally:
+            new_list.append(c)
     return (new_list)
