97c2c5d911958a460b0696c2a996c8949de490ab
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-29 20:03:41 +0300

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,17 +5,20 @@ Write a class Square that defines a square by: (based on 1-square.py)
 
 Private instance attribute: size
 Instantiation with optional size: def __init__(self, size=0):
-size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
+
+size must be an integer,
+otherwise raise a TypeError exception with the message size must be an integer
+
+if size is less than 0,
+raise a ValueError exception with the message size must be >= 0
 You are not allowed to import any module
 """
+
+
 class Square:
     """Write a class Square that defines a square by: (based on 1-square.py)"""
     def __init__(self, size=0):
-        try:
-            self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
-
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
