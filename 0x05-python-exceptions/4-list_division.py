f27bdc80c8047756c9578312903438058d2baac9
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-30 11:47:28 +0300

    correct indexing vlaue calculation in task 4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,13 +6,14 @@ def list_division(my_list_1, my_list_2, list_length):
             a = my_list_1[idx]
             b = my_list_2[idx]
             c = a / b
+        except IndexError:
+            print("out of range")
+            c = 0
         except TypeError:
             print("wrong type")
         except ZeroDivisionError:
             c = 0
             print("division by 0")
-        except IndexError:
-            print("out of range")
         finally:
             new_list.append(c)
     return (new_list)
