8092b4da586d07c97d7f3769d98b4675d8ba1794
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-30 05:07:53 +0300

    fix string in tuple in :: 6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,6 +73,7 @@ class Square:
     def position(self, value):
         if not isinstance(value, tuple) or \
             len(value) != 2 or \
+                not all(isinstance(num, int) for num in value) or \
                 not all(num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
