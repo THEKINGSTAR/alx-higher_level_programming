0177a24fd1f3e5bb0c0636650335302ca3b133d7
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-30 04:08:26 +0300

    4. Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,17 +1,48 @@
+#!/usr/bin/python3
+
 """
 Write a class Square that defines a square by: (based on 3-square.py)
 
 Private instance attribute: size:
+
 property def size(self): to retrieve it
+
 property setter def size(self, value): to set it:
-size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
+size must be an integer,
+otherwise raise a TypeError exception with the message size must be an integer
+
+if size is less than 0,
+   raise a ValueError exception with the message size must be >= 0
+
 Instantiation with optional size: def __init__(self, size=0):
-Public instance method: def area(self): that returns the current square area
+
+Public instance method: def area(self):
+    that returns the current square area
 You are not allowed to import any module
-Why?
-
-Why a getter and setter?
-
-Reminder: size is a private attribute. We did that to make sure we control the type and value. Getter and setter methods are not 100% Python, but more OOP. With them, you will be able to validate the assignment of a private attribute and also define how getting the attribute value will be available from outside - by copy? by assignment? etc. Also, adding type/value validation in the setter will centralize the logic, since you will do it in only one place.
 """
+
+
+class Square:
+    """Write a class Square that defines a square by: (based on 1-square.py)"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    """
+    Public instance method: def area(self):
+        that returns the current square area
+    You are not allowed to import any module
+    """
+    def area(self):
+        return (self.__size ** 2)
