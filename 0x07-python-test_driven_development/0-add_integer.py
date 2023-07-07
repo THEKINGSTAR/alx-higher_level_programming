1da9639ecb56f98935ba2475bb88c30c9b75d554
THEKINGSTAR <thekingstar5002@gmail.com>
2023-07-07 22:43:21 +0300

    0. Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is the "add" module.supplies one function, add_integer().  For example,
+>>> add_integer(5, 6)
+11
+"""
+
+def add_integer(a, b=98):
+    """
+    Returns an integer: the addition of a and b
+    """
+    sum = 0
+    try:
+        f = int(a)
+        s = int(b)
+    except:
+        raise TypeError("a must be an integer or b must be an integer")
+        return
+    sum = int(f + s)
+    return (sum)
