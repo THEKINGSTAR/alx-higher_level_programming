fcfad11d32b526160d8832613376b6063fc2b601
THEKINGSTAR <thekingstar5002@gmail.com>
2023-07-08 05:23:53 +0300

    check all test in qa

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,11 +11,11 @@ def add_integer(a, b=98):
     Returns an integer: the addition of a and b
     """
     sum = 0
-    try:
-        f = int(a)
-        s = int(b)
-    except Exception:
+    if type(a) is not int:
         raise TypeError("a must be an integer or b must be an integer")
         return
-    sum = int(f + s)
+    if type(b) is not int:
+        raise TypeError("a must be an integer or b must be an integer")
+        return
+    sum = int(a + b)
     return (sum)
