868021980339c2248dbf115dc1951d77f7804927
THEKINGSTAR <thekingstar5002@gmail.com>
2023-07-08 05:49:54 +0300

    fix test and results message of integers and strings

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,10 +12,10 @@ def add_integer(a, b=98):
     """
     sum = 0
     if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("a must be an integer")
         return
     if type(b) is not int and type(b) is not float:
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("b must be an integer")
         return
     if type(a) is float:
         a = int(a)
