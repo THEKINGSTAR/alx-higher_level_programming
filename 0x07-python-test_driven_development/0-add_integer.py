bdef7c5c86e56bbc46945be88058839f45808dbd
THEKINGSTAR <thekingstar5002@gmail.com>
2023-07-08 05:30:48 +0300

    fix float convert , non exsisting number

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,11 +11,15 @@ def add_integer(a, b=98):
     Returns an integer: the addition of a and b
     """
     sum = 0
-    if type(a) is not int:
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer or b must be an integer")
         return
-    if type(b) is not int:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("a must be an integer or b must be an integer")
         return
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
     sum = int(a + b)
     return (sum)
