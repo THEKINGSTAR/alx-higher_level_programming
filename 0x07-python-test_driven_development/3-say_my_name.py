1fc55b34ac1ee733a37a04ba12aff067c5b822da
THEKINGSTAR <thekingstar5002@gmail.com>
2023-07-08 05:05:25 +0300

    2. Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,6 +10,9 @@ My name is John Smith$
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    print first or last name
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
