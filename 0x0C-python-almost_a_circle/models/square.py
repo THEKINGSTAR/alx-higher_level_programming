c5ef3e3f212eada553b683cb6e10f402f80995cf
THEKINGSTAR <thekingstar5002@gmail.com>
2023-07-18 00:39:38 +0300

    update the setter methods

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,15 +25,11 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(size, size, x, y, id)
-        """
         self.size = size
         self.x = x
         self.y = y
-        """
-    
-    def __str__(self):
-        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
- 
+        self.id = id
+
     @property
     def size(self):
         """adding the public getter and setter size"""
@@ -80,3 +76,6 @@ class Square(Rectangle):
             self.x = kwargs["x"]
         if kwargs.get("y"):
             self.y = kwargs["y"]
+    
+    def __str__(self):
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
