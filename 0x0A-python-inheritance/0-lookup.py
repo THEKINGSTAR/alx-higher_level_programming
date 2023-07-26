27dc54b2cc364b0f8112a48125e30933fe47fb29
THEKINGSTAR <thekingstar5002@gmail.com>
2023-07-26 23:05:39 +0300

    0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
+"""
+this module containe one function lookup
+that list all the atribute of calss
+"""
+
+
 def lookup(obj):
+    """
+    class obj to look for its atributes
+    """
     at_lst = []
     for ob in dir(obj):
         at_lst.append(ob)
