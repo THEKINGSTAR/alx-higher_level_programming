78752d2f294a123b1e738caa904b8fa33e58fbd4
THEKINGSTAR <thekingstar5002@gmail.com>
2023-07-31 18:11:29 +0300

    FINISH TASK # 6. How many instances

---
+++ 0x08-python-more_classes/6-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+Rectangle = __import__('6-rectangle').Rectangle
+
+my_rectangle_1 = Rectangle(2, 4)
+my_rectangle_2 = Rectangle(2, 4)
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_1
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_2
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+
+"""
+guillaume@ubuntu:~/0x08$ ./6-main.py
+2 instances of Rectangle
+Bye rectangle...
+1 instances of Rectangle
+Bye rectangle...
+0 instances of Rectangle
+guillaume@ubuntu:~/0x08$ 
+"""
