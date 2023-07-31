a95c5daa900e1b93dc69d6951f008f70a0b05130
THEKINGSTAR <thekingstar5002@gmail.com>
2023-07-31 15:15:59 +0300

    TASK # 3. String representation

---
+++ 0x08-python-more_classes/3-main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+Rectangle = __import__('3-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle)
+print(repr(my_rectangle))
+
+"""
+guillaume@ubuntu:~/0x08$ ./3-main.py
+Area: 8 - Perimeter: 12
+##
+##
+##
+##
+<3-rectangle.Rectangle object at 0x7f92a75a2eb8>
+--
+##########
+##########
+##########
+<3-rectangle.Rectangle object at 0x7f92a75a2eb8>
+guillaume@ubuntu:~/0x08$ 
+"""
