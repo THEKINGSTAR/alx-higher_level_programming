c69a6455cf753fc83f65a93c9d18cd3ec62dec0e
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-16 23:15:27 +0300

    FIX TASK # 0:

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- a script that lists all privileges of the MySQL users
 -- user_0d_1 and user_0d_2 on your server (in localhost).
+FLUSH PRIVILEGES;
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
