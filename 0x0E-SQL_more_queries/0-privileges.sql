c1b07be73b8a0fed54c2dd94a23f40f5194ef69d
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-16 23:38:25 +0300

    4. ID can't be null

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- a script that lists all privileges of the MySQL users
--- user_0d_1 and user_0d_2 on your server (in localhost).
-FLUSH PRIVILEGES;
+-- user_0d_1 and user_0d_2 on your server
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
