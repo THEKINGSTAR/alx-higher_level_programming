5f85be893e6dfe35b716fd71c3f2e5004b879fa4
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-16 03:19:44 +0300

    5. Full description

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -4,4 +4,4 @@
 -- name VARCHAR(256)
 -- The database name will be passed as an argument of the mysql command
 -- If the table first_table already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS `first_tabel`(`id` int,`name` varchar(256));
+CREATE TABLE IF NOT EXISTS  `first_table`(`id` int,`name` varchar(256));
