cd88a5a1f06eb7ff095aa1f295c89498ef26bf8e
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-17 21:37:06 +0300

    9. Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,12 @@
+-- a script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+-- cat 9-cities_by_state_join.sql | mysql -hlocalhost -uroot -p hbtn_0d_usa
+
+SELECT cities.id, cities.name , states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC
