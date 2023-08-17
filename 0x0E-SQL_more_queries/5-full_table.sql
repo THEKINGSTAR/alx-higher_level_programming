c03e1de42052532a5614cd8676ea249c7ef68079
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-17 07:38:23 +0300

    6. States table

---
+++ 0x0E-SQL_more_queries/5-full_table.sql
@@ -0,0 +1,8 @@
+-- a script that prints the full description of the table first_table
+-- from the database hbtn_0c_0 in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+
+-- SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
+-- FROM INFORMATION_SCHEMA.COLUMNS
+-- WHERE TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
