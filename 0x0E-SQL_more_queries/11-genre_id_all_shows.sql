b7ceabb3092483c1dc344695ac5a2381b1c9c25f
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-18 00:06:21 +0300

    CORRECT TASK # 11. Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,7 +5,12 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title, tv_show_genres.genre_id
+-- SELECT tv_shows.title, tv_show_genres.genre_id
+-- FROM tv_shows
+-- INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+-- ORDER BY tv_shows.title, tv_show_genres.genre_id;
+
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
