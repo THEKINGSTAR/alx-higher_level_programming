b3dde027508a0bf5f12db96f31e2156581423c8e
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-18 23:01:35 +0300

    CORRECT TASK # 13. Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -8,10 +8,18 @@
 -- The database name will be passed as an argument of the mysql command
 -- cat 13-count_shows_by_genre.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
 
-SELECT tv_genres.name AS genre, COUNT(tv_genres.id) AS number_of_shows
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-LEFT JOIN tv_shows ON  tv_shows.id = tv_show_genres.show_id
-GROUP BY genre
-ORDER BY number_of_shows DESC;
 
+-- ---------------- MY ORIGINAL QUERY -------------------------------------
+-- SELECT tv_genres.name AS genre, COUNT(tv_genres.id) AS number_of_shows
+-- FROM tv_genres
+-- LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+-- LEFT JOIN tv_shows ON  tv_shows.id = tv_show_genres.show_id
+-- GROUP BY genre
+-- ORDER BY number_of_shows DESC;
+-- -------------------------------------------------------------------------
+
+SELECT tv_genres.name As genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres 
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+GROUP BY tv_show_genres.genre_id 
+ORDER BY number_of_shows DESC;
