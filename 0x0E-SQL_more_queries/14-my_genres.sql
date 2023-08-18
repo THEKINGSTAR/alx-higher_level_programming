5119beb1595cbf972865c152ec62d929820ced03
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-18 23:05:44 +0300

    CORRECT TASK # 14. My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,7 +5,17 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
+-- ----------------------- ORIGINAL SCRIPT -------------------------------
 SELECT name FROM tv_genres
 LEFT JOIN tv_show_genres  ON tv_genres.id =  tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE title = 'Dexter';
+ORDER BY tv_genres.name ASC;
+-- ----------------------------------------------------------------------
+
+-- SELECT tv_genres.name 
+-- FROM tv_show_genres 
+-- INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id 
+-- INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
+-- WHERE tv_shows.title = 'Dexter' 
+-- ORDER BY tv_genres.name ASC;
