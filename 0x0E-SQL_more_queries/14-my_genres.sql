4084231274ffc3146c3c1aaa6ebabd0c62726259
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-18 23:07:34 +0300

    RECORRECT TASK # 14. My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -9,8 +9,8 @@
 SELECT name FROM tv_genres
 LEFT JOIN tv_show_genres  ON tv_genres.id =  tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE title = 'Dexter';
-ORDER BY tv_genres.name ASC;
+WHERE title = 'Dexter'
+ORDER BY name ASC;
 -- ----------------------------------------------------------------------
 
 -- SELECT tv_genres.name 
