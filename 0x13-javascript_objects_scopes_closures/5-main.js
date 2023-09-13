2a845acebb3d7813741800be07f1c073895324a8
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-13 18:48:22 +0300

    5. Square #0

---
+++ 0x13-javascript_objects_scopes_closures/5-main.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+const s1 = new Square(4);
+s1.print();
+s1.double();
+s1.print();
+
+/*
+guillaume@ubuntu:~/0x13$ ./5-main.js
+XXXX
+XXXX
+XXXX
+XXXX
+XXXXXXXX
+XXXXXXXX
+XXXXXXXX
+XXXXXXXX
+XXXXXXXX
+XXXXXXXX
+XXXXXXXX
+XXXXXXXX
+guillaume@ubuntu:~/0x13$ 
+*/
