c8643754b06212f5f35c4f1e7196beafdae9da1c
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-14 22:03:26 +0300

    5. An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const x = parseInt(args[2], 10);
+
+if (!Number.isInteger(x) || typeof x === 'string') {
+  console.log('Not a number');
+} else {
+  console.log('My number:' + x);
+}
