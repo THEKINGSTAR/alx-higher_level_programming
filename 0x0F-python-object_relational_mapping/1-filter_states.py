91f2c54acf2f70a99286f52d7b5f24eb00d18784
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-16 14:22:28 +0300

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
+"""
+MODULE TO  lists all states from the database hbtn_0e_0_usa:
+"""
+if __name__ == "main":
+    import MySQLdb
+    import sys
 
-import MySQLdb
-import sys
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", user=mysql_username,
+                         port=3307, password=mysql_password,
+                         database=database_name)
 
-db = MySQLdb.connect(host="localhost",user = mysql_username, port = 3307,password = mysql_password, database = database_name)
+    cur = db.cursor()
 
-db.query("""SELECT states FROM hbtn_0e_0_usa
-         WHERE name "like "N.%""")
+    query = """SELECT *  FROM states
+            WHERE name like BINARY 'N%'
+            ORDER BY states.id ASC;"""
 
+    cur.execute(query)
 
+    results = cur.fetchall()
+
+    for row in results:
+        print(row)
+
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/test_script.py
+++ 0x0F-python-object_relational_mapping/test_script.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
+
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host='localhost', port=3306,
-                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+mysql_username = sys.argv[1]
+mysql_password = sys.argv[2]
+database_name = sys.argv[3]
+
+db = MySQLdb.connect(host="localhost", user=mysql_username,
+                        port=3307, password=mysql_password,
+                        database=database_name)
 
 cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-rows = cur.fetchall()
-for row in rows:
+
+query = """SELECT *  FROM states
+        WHERE name like BINARY 'N%'
+        ORDER BY states.id ASC;"""
+
+cur.execute(query)
+
+results = cur.fetchall()
+
+for row in results:
     print(row)
+
+cur.close()
+db.close()
