62f89a9a1eb34200a8ff58fc64e0da18a46d6cdb
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-16 03:43:18 +0300

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
+"""
+MODULE TO  lists all states from the database hbtn_0e_0_usa:
+"""
+if __name__ == "main":
+    import MySQLdb
+    import sys
 
-import MySQLdb
-import sys
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", user=mysql_username,
+                         port=3307, password=mysql_password,
+                         database=database_name)
 
-db = MySQLdb.connect(host="localhost", user=mysql_username,
-                     port=3307, password=mysql_password,
-                     database=database_name)
+    cur = db.cursor()
 
-cur = db.cursor()
+    query = """SELECT name FROM states
+            ORDER BY id ASC"""
+    cur.execute(query)
 
-query = """SELECT name FROM states
-           ORDER BY id ASC"""
-cur.execute(query)
+    results = cur.fetchall()
 
-results = cur.fetchall()
+    for idx, row in enumerate(results, start=1):
+        print("({}, '{}')".format(idx, row[0]))
 
-for idx, row in enumerate(results, start=1):
-    print("({}, '{}')".format(idx, row[0]))
-
-cur.close()
-db.close()
+    cur.close()
+    db.close()
