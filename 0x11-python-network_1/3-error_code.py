07901e64f2aab17bedb030a05a75081d8c91621d
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-30 01:27:12 +0300

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 
 """
-Python script that takes in a URL,
+Python script that takes in a URL
 sends a request to the URL
-and
-displays the body of the response (decoded in utf-8).
+displays the body of the response (decoded in utf-8)
 You have to manage urllib.error.HTTPError exceptions
-and
 print: Error code: followed by the HTTP status code
-You must use the packages urllib and sys
-You are not allowed to import other packages than urllib and sys
-You don’t need to check arguments passed to the script (number or type)
-You must use the with statement
 """
 
-import urllib.request
-import urllib.parse
-import sys
 
+if __name__ == "__main__":
+    """
+    displays the body of the response (decoded in utf-8)
+    """
 
-try:
-    args = sys.argv
-    
-    with urllib.request.urlopen(args[1]) as response:
-        response_body = response.read().decode('utf-8')
-        print(response_body)
+    import urllib.request
+    import urllib.parse
+    import urllib.error
+    import sys
 
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
+    try:
+        args = sys.argv
+        link = args[1]
+        with urllib.request.urlopen(url=link) as response:
+            response_body = response.read().decode('utf-8')
+            print(response_body)
+
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
