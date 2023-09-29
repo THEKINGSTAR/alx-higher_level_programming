48cf02fab529ddd0848b11d2cd1b80dd097d5c0b
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-30 02:19:51 +0300

    6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL, sends a request to the URL """
+"""
+Python script that takes in a URL and an email address
+sends a POST request to the passed URL
+"""
 
 
 if __name__ == "__main__":
     """
-    sends a POST request to the passed URL
-    with the email as a parameter
-    and finally displays the body of the response.
+    The email must be sent in the variable email
+    You must use the packages requests and sys
+    You are not allowed to import packages other than requests and sys
     """
 
     import requests
@@ -16,9 +19,9 @@ if __name__ == "__main__":
         args = sys.argv
         u_r_l = args[1]
         email = args[2]
-        response = requests.get(sys.argv[1])
-        answer = response.headers['X-Request-Id']
-        print(answer)
+        data = {'email': email}
+        response = requests.post(url=u_r_l, data=data)
+        print(response.text)
 
     except Exception as e:
         print(e)
