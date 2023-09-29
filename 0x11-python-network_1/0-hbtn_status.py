7b539e40aad1abe55f36a192ccd24d443d371846
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-29 14:11:47 +0300

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
+
+import urllib.request
+
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+   html = response.read()
+print("Body response:$")
+for ln in html:
+    print("     - {}".format(ln))
