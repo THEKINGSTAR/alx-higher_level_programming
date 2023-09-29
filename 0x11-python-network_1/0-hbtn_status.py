848b3383e51409d3da768c7bc31f43b0914f98bc
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-29 14:15:23 +0300

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,5 @@ import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
 print("Body response:")
-print("\t- type:", type(html_content))
-print("\t- content:", html_content.decode('utf-8'))
+print("\t- type:", type(html))
+print("\t- content:", html.decode('utf-8'))
