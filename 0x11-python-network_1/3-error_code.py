28aee89de6333fcbd23a17c8b34189f1337bccc1
THEKINGSTAR <thekingstar5002@gmail.com>
2023-09-29 23:39:40 +0300

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 """
-Python script that takes in a URL and an email,
-sends a POST request to the passed URL with the email as a parameter,
+Python script that takes in a URL,
+sends a request to the URL
 and
-displays the body of the response (decoded in utf-8)
-The email must be sent in the email variable
+displays the body of the response (decoded in utf-8).
+You have to manage urllib.error.HTTPError exceptions
+and
+print: Error code: followed by the HTTP status code
 You must use the packages urllib and sys
-You are not allowed to import packages other than urllib and sys
+You are not allowed to import other packages than urllib and sys
 You don’t need to check arguments passed to the script (number or type)
 You must use the with statement
 """
@@ -21,12 +23,9 @@ args = sys.argv
 
 try:
     u_r_l = args[1]
-    data =  urllib.error.HTTPError(u_r_l).encode('utf-8')
-    if 'HTTPError' in  data:
-
-    with urllib.request.urlopen(url=u_r_l, data=data) as response:
+    with urllib.request.urlopen(u_r_l) as response:
         response_body = response.read().decode('utf-8')
         print(response_body)
 
-except Exception as e:
-    print(e)
+except urllib.error.HTTPError as e:
+    print(f"Error code: {e.code}")
