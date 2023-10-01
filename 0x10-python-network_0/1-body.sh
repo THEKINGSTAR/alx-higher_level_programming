f515269a521642ea4be7c5588182a3c9fd62e541
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-01 21:04:48 +0300

    1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-echo curl -s -w "%{http_code}" "$1" | tail -c 3
+curl -s -w "%{http_code}" "$1" | tail -c 3
