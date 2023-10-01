072ec5f835bf55142a76c3914dfa824ba2a4b1a4
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-01 21:57:05 +0300

    9. Catch me if you can push

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request causes the server to respond with a message containing You got me!
-curl -s  0.0.0.0:5000/catch_me 1>&2
+curl -o /dev/null -sL  -X POST "" 0.0.0.0:5000/catch_me 1>&2
