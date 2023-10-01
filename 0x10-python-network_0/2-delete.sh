1c91beaeaa030d4680a1509e2f55512173e0b7e4
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-01 22:46:55 +0300

    2. cURL Method

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL and displays all HTTP methods the server will accept.
+#Bash script sends a DELETE request to URL passed as the first argument and displays the body of the response
 curl -X DELETE "$1"
