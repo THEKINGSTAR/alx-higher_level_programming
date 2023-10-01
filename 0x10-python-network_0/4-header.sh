4513e1c60b9a86f87d608e2e1ea6ad4b9480077d
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-01 22:14:59 +0300

    4. cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script takes in a URL as argument,sends a GET request to the URL,displays the body of the response
-curl -s  -X GET -d "X-School-User-Id=98" "$1"
+curl -s  -X GET -H "X-School-User-Id:98" "$1"
