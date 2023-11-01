b7727f7ed61159e0ae49220dcb6f1cfe54e9871e
THEKINGSTAR <thekingstar5002@gmail.com>
2023-11-02 01:28:42 +0200

    task 0. No JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+//JavaScript script that updates the text color of the <header> element to red (#FF0000):
+const header_text = document.querySelector('headr');
+header_text.ariaValueText=("#FF0000");
