153d11bdd31c342033ad507e4778abe29191b068
THEKINGSTAR <thekingstar5002@gmail.com>
2023-11-02 14:48:18 +0200

    6. Change the text

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,12 +1,10 @@
 
 /* eslint-disable */
 /*
-- Write a JavaScript script that adds
- a <li> element to a list when the user clicks on the tag DIV#add_item:
-- The new element must be: <li>Item</li>
-- The new element must be added to UL.my_list
-- You can’t use document.querySelector to select the HTML tag
-- You must use the JQuery API
+Write a JavaScript script that updates the
+text of the <header> element to New Header!!! when the user clicks on DIV#update_header
+You can’t use document.querySelector to select the HTML tag
+You must use the JQuery API
 */
 
 $(document).ready(function () {
---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,22 @@
+
+/* eslint-disable */
+/*
+Write a JavaScript script that fetches the character name
+from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
+The name must be displayed in the HTML tag DIV#character
+You can’t use document.querySelector to select the HTML tag
+You must use the JQuery API
+*/
+
+$(document).ready(function () {
+
+    $('DIV#character').click(function () {
+        const content = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
+        $.get(content, function(data)
+        {
+            const contentName = data.name;
+            $('DIV#character').text(contentName);
+        });
+    });
+  });
+  
