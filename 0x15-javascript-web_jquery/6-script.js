e95b4f8684ed2dfdce5825d55bb7bc677bc04019
THEKINGSTAR <thekingstar5002@gmail.com>
2023-11-02 14:34:58 +0200

    6. Change the text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+/*
+- Write a JavaScript script that adds 
+ a <li> element to a list when the user clicks on the tag DIV#add_item:
+- The new element must be: <li>Item</li>
+- The new element must be added to UL.my_list
+- You can’t use document.querySelector to select the HTML tag
+- You must use the JQuery API
+*/
+
+$(document).ready(function () {
+    $('DIV#update_header').click(function () {
+      const header = $('header');
+        header.text('New Header!!!');
+    });
+  });
+  
