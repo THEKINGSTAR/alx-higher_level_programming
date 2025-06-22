c162b909fd3a6acdf87c283f1eb896cf76741728
THEKINGSTAR <thekingstar5002@gmail.com>
2025-06-23 02:50:08 +0300

    add files

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 /* Script that displays the status code of a GET request */
-
+/*
+ * The first argument is the URL to request (GET)
+ * The status code must be printed like this: code: <status code>
+ * You must use the module request
+ */
 const https = require('https');
 const url = process.argv[2];
 
