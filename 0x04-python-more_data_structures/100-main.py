2445aa58e5864603b2565f1503dc4243e7a3b0fd
THEKINGSTAR <thekingstar5002@gmail.com>
2025-07-04 20:51:34 +0300

    13. Weighted average

--- 0x04-python-more_data_structures/100-main.py
+++ 0x04-python-more_data_structures/100-main.py
@@ -5,3 +5,8 @@ my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
 # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
 result = weight_average(my_list)
 print("Average: {:0.2f}".format(result))
+
+
+list = []
+result = weight_average(list)
+print("Av: {:0.2f}".format(result))
--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    avg, sumtn, demon = 0, 0, 0
     if my_list is None or len(my_list) <= 0:
-        return None
+        return 0
+    avg, sumtn, demon = 0, 0, 0
     for itm in my_list:
         sumtn += (itm[1] * itm[0])
         demon += itm[1]
