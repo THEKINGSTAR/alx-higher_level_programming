672004959a0e7395a9220cecb0a65a5b0e1c6282
THEKINGSTAR <thekingstar5002@gmail.com>
2025-07-04 04:57:39 +0300

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,8 +6,32 @@ def roman_to_int(roman_string):
     '''
     if roman_string is None or not isinstance(roman_string, str):
         return None
-    roman={'I': 1, 'V':5, "X":10, "L":50	, "C":100, "D":500, "M":1000}
+    roman = {'I': 1, 'V': 5, "X": 10,
+             "L": 50, "C": 100, "D": 500,
+             "M": 1000}
     rom_dec = 0
-    for digt in roman_string:
-        rom_dec += roman[digt]
+    roman_string = roman_string.upper()
+
+    for idx, digt in enumerate(roman_string):
+        if idx + 1 < len(roman_string):
+            if roman[digt] < roman[roman_string[idx + 1]]:
+                # print(f"Subtrac: {roman[digt]} from {roman_string[idx+1]}")
+                rom_dec -= roman[digt]
+            else:
+                # print(f"Adding {roman[digt]} to total")
+                rom_dec += roman[digt]
+        else:
+            # print(f"Adding {roman[digt]} to total")
+            rom_dec += roman[digt]
     return rom_dec
+
+
+print(roman_to_int("XIX"))  # Example usage, should return 19:
+print(roman_to_int("XLII"))  # Example usage, should return 42:
+print(roman_to_int("MCMXCIV"))  # Example usage, should return 1994
+print(roman_to_int("MMMCMXCIX"))  # Example usage, should return 3999
+print(roman_to_int("MMXXIII"))  # Example usage, should return 2023
+print(roman_to_int("MMXXIV"))  # Example usage, should return 2024
+print(roman_to_int("MMXXV"))  # Example usage, should return 2025
+print(roman_to_int("MMXXVI"))  # Example usage, should return 2026
+print(roman_to_int("MMXXVII"))  # Example usage, should return 2027
